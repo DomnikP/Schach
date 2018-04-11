51c9b8514837b3d8d75c5c0bd19c9d78e69bb545
DomnikP <Dominik-Papke@web.de>
2018-04-11 14:51:34 +0200

    Implemented the Moce, NormalMove, and AttackingMove classes

--- src/com/Schach/Board/Move.java
+++ src/com/Schach/Board/Move.java
@@ -1,4 +1,38 @@
 package com.Schach.Board;
 
-public class Move {
+import com.Schach.Pieces.Piece;
+
+public abstract class Move {
+
+    final Board board;
+    final Piece movedPiece;
+    final int destinationCoordinate;
+
+    private Move(final Board board, final Piece movedPiece, final int destinationCoordinate) {
+
+        this.board = board;
+        this.movedPiece = movedPiece;
+        this.destinationCoordinate = destinationCoordinate;
+    }
+
+    public static final class NormalMove extends Move {
+
+
+        public NormalMove(Board board, Piece movedPiece, int destinationCoordinate) {
+            super(board, movedPiece, destinationCoordinate);
+        }
+    }
+
+    public static final class AttackMove extends Move {
+
+        final Piece attackedPiece;
+
+
+        public AttackMove(Board board, Piece movedPiece, int destinationCoordinate, final Piece attackedPiece) {
+            super(board, movedPiece, destinationCoordinate);
+            this.attackedPiece=attackedPiece;
+        }
+    }
+
+
 }
